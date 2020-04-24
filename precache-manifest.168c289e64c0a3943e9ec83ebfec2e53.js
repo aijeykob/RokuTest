5f7f08a06707cb19018f609618f4723306d37f85
AiJey <aijeykob@gmail.com>
2020-04-24 12:56:21 +0300

    Updates

---
+++ precache-manifest.168c289e64c0a3943e9ec83ebfec2e53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48706aa0acbe0ab3162c0cdf9570fb83",
+    "url": "/RokuTest/index.html"
+  },
+  {
+    "revision": "90dd2a9b26f4f2092833",
+    "url": "/RokuTest/static/css/main.0d320229.chunk.css"
+  },
+  {
+    "revision": "ab7600c555fded2d2fa4",
+    "url": "/RokuTest/static/js/2.d5097c89.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RokuTest/static/js/2.d5097c89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90dd2a9b26f4f2092833",
+    "url": "/RokuTest/static/js/main.d9b79aa1.chunk.js"
+  },
+  {
+    "revision": "73d25a483e6ca71caca8",
+    "url": "/RokuTest/static/js/runtime-main.47381699.js"
+  }
+]);
